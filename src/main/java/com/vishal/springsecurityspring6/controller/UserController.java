115879f6c3aa22eb743518b404f6c3572626e57f
Vishal Soni <50075856+vishalsoni008@users.noreply.github.com>
2023-01-08 15:29:41 +0530

    know about csrf

--- src/main/java/com/vishal/springsecurityspring6/controller/UserController.java
+++ src/main/java/com/vishal/springsecurityspring6/controller/UserController.java
@@ -1,10 +1,14 @@
 package com.vishal.springsecurityspring6.controller;
 
+import com.vishal.springsecurityspring6.model.User;
 import com.vishal.springsecurityspring6.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,7 +19,12 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/")
-    private ResponseEntity allUsers() {
+    public ResponseEntity allUsers() {
         return ResponseEntity.ok(userService.findAllUsers());
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity addUser(@RequestBody User user) {
+        return new ResponseEntity(userService.addUser(user), HttpStatus.CREATED);
     }
 }
